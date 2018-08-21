305505545ff7a33461cfd752b4e788e3202131e8
Frederik Klovborg <klovborg@gmail.com>
2018-08-21 11:45:12 +0200

    cron-every-minute-0-of-12th-hour

--- server/server.js
+++ server/server.js
@@ -15,8 +15,11 @@ const app = express()
 app.use(bodyParser.json())
 
 
-nodeCron.schedule('0,30,39 */1 * * *', () => {
-  console.log('Job running every minute 0, 30, 39 of every hour', new Date())
+nodeCron.schedule('0 */12 * * *', () => {
+  // Log to monitor job
+  console.log('Job running every minute 0 of 12th hour', new Date())
+  
+  // Fetch latest USD exchange rates from CurrencyLayer
   fetchCurrencyLayer((err, res) => {
     if(err) {
       console.log('Something went wrong with fetching currency from CurrencyLayer', err)
@@ -29,24 +32,19 @@ nodeCron.schedule('0,30,39 */1 * * *', () => {
       })
     }
   })
+
+  // Fetch latest Euro exchange rates from Fixer.io
   fetchFixer((err, res) => {
     if(err) {
-      return console.log('Something went wrong with fetching currency from Fixer', err)
+      return console.log('Something went wrong with fetching currency from fixer.io', err)
+    } else {
+      Currency.findOneAndUpdate({ source: 'EUR' }, res, (err, update) => {
+        if(err) {
+          return console.log('Could not update EUR currency document', err)
+        }
+        console.log('Updated EUR currency document', update)
+      })
     }
-    const currency = new Currency(res)
-
-    currency.save().then(console.log(currency))
-  })
-})
-
-// Consider removing this at some point soon... Should only be for initializing the db (which have already been done)
-app.post('/currencies', (req, res) => {
-  const currency = new Currency(req.body.currency)
-
-  currency.save().then(doc => {
-    res.send(doc)
-  }, e => {
-    res.status(400).send(e)
   })
 })
 
