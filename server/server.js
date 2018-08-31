d682bb25f186ff65b116fe2aa2a51bef4e028d8f
Frederik Klovborg <klovborg@gmail.com>
2018-08-31 16:44:51 +0200

    remove-cron-job

--- server/server.js
+++ server/server.js
@@ -7,15 +7,11 @@ const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const nodeCron = require('node-cron')
 
 const { mongoose } = require('./db/mongoose')
 const { Currency } = require('./models/currency')
 const { User } = require('./models/user')
 const { authenticate } = require('./middleware/authenticate')
-
-const { fetchCurrencyLayer } = require('./utils/fetchCurrencyLayer')
-const { fetchFixer } = require('./utils/fetchFixer') 
 
 const { schema } = require('./schema')
 
@@ -30,39 +26,6 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
-
-nodeCron.schedule('0 */12 * * *', () => {
-  // Log to monitor job
-  console.log('Job running every minute 0 of 12th hour', new Date())
-  
-  // Fetch latest USD exchange rates from CurrencyLayer
-  fetchCurrencyLayer((err, res) => {
-    if(err) {
-      console.log('Something went wrong with fetching currency from CurrencyLayer', err)
-    } else {
-      Currency.findOneAndUpdate({ source: 'USD' }, res, (err, update) => {
-        if(err) {
-          return console.log('Could not update USD currency document', err)
-        }
-        console.log('Updated USD currency document', update)
-      })
-    }
-  })
-
-  // Fetch latest Euro exchange rates from Fixer.io
-  fetchFixer((err, res) => {
-    if(err) {
-      return console.log('Something went wrong with fetching currency from fixer.io', err)
-    } else {
-      Currency.findOneAndUpdate({ source: 'EUR' }, res, (err, update) => {
-        if(err) {
-          return console.log('Could not update EUR currency document', err)
-        }
-        console.log('Updated EUR currency document', update)
-      })
-    }
-  })
-})
 
 app.get('/currencies', (req, res) => {
   Currency.find().then(currencies => {
